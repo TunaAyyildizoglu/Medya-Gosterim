8c25fb5a97c5db528204657b6d8d734bb75e45bc
Tuna <tuna.ayyildizoglu@gmail.com>
2021-10-20 00:23:45 +0300

    Proje Düzeltilip Eklendi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-images = ["resources/images/1.png", "resources/images/2.png", "resources/images/3.png","resources/images/4.png","resources/images/5.png"];
+// resimleri ve sesleri burada dizi içinde tanımladım 
+images = ["resources/images/1.png", "resources/images/2.png", "resources/images/3.png","resources/images/4.png","resources/images/5.png"]; 
 audios = ["resources/audios/1.mp3", "resources/audios/2.mp3"]
 
+//resim ve ses id'lerini değiştirelemez şeklinde const ile tanımladım
 const imageContent = document.getElementById("imageContent");
 const audioContent = document.getElementById("audioContent");
 
@@ -14,13 +16,13 @@ function initializeContentButtons()
         loadAudioFunc = "loadAudio(" + i % 2 + ")"; // elimizde 2 adet ses dosyası var
         contentButtons[i].setAttribute("onclick", loadImageFunc + loadAudioFunc);
 
-        contentButtons[i].innerHTML = (i + 1) + ". İçerik";
+        contentButtons[i].innerHTML = (i + 1) + ". İçerik";//html kısmına innerHtml kodu ile içeriklerimizi yazdırıyoruz
     }
 }
 
 initializeContentButtons();
 
-
+// eğer butona tıklandı ise contentbutton'a buton tıklandı class'ını ekliyoruz
 function loadImage(contentIndex, event)
 {
     imageContent.src = images[contentIndex];
@@ -31,6 +33,7 @@ function loadImage(contentIndex, event)
     checkAllClickedState();
 }
 
+//bütün butonlara tıklanıp tıklanmadığını kontrol edip ileri butonunu görünür hale getirdim
 function checkAllClickedState()
 {
     query = document.querySelectorAll(".contentButton:not(.buttonClicked)");
@@ -41,6 +44,7 @@ function checkAllClickedState()
     }
 }
 
+// ses butonlarını aktif etme
 function loadAudio(contentIndex)
 {
     audioButtons = document.getElementsByClassName("audioButton");
@@ -54,6 +58,7 @@ function loadAudio(contentIndex)
     audioPlayStop('Play');
 }
 
+// ses dosyalarının başlatma ve durdurma işlemini bu fonksiyonda gerçekleştirdim
 function audioPlayStop(PlayStop)
 {
     if(PlayStop == 'Play')
